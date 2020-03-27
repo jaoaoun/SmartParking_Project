1bb1893fab7226e5783cd01a62d41dd848abbdf9
jaoaoun <ntd_pst@hotmail.com>
2020-03-27 17:23:43 +0700

    3/27/2020 : Try to button click to next page, use Link to next page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import HelloApp from './components/HelloApp'
-import Home from './components/home'
+import Home from './components/Home'
 
-export default function App() {
-       return  <Home />
+function App() {
+  /* Hello */
+
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/HelloApp">
+            <HelloApp />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
+
+export default App
--- src/components/home.js
+++ src/components/home.js
@@ -1,23 +1,34 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router'
+/*import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useHistory,
+  useLocation
+} from "react-router-dom"
+*/
 
-class Home extends Component {
-  handleClick() {
-    console.log('aoun')
+export default function HelloApp() {
+  
+  const { push } = useHistory()
+
+  const handleClick = () => {
+    push('./Home.js')
   }
 
-  render() {
-    return (
-      <div className="home">
-        <div className="home-header">
-          <img src={require('../pkc.gif')} alt="logo" className="home-logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="home-intro">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-
-        <button onClick={this.handleClick}>Next!</button>
+  return (
+    <div className="home">
+      <div className="home-header">
+        <img src={require('../pkc.gif')} alt="logo" className="home-logo" />
+        <h2>Welcome to React</h2>
       </div>
-    )
-  }
+      <p className="home-intro">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+      <button onClick={handleClick}>Next</button>
+      <Link to="/HelloApp">HelloApp</Link>
+    </div>
+  )
 }
-
-export default Home

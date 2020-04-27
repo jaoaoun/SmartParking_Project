0032664519d3250bed807cb20b60e405815ef07d
o.srч <sorayuth10@gmail.com>
2020-04-27 17:20:57 +0700

    change path to config firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import * as firebase from 'firebase'
+import { firebaseConfig } from './config'
+firebase.initializeApp(firebaseConfig)
 
 ReactDOM.render(
   <App />,

b972a175c8c70b71bb56dac60d3ab47913003600
o.srч <sorayuth10@gmail.com>
2020-04-27 17:09:10 +0700

    useState component push to another page fixed

--- src/components/layout/HeaderBar.js
+++ src/components/layout/HeaderBar.js
@@ -12,98 +12,85 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
-  Button,
-  Container,
-  Row,
-  Col
+  Button
 } from 'reactstrap'
 
-
-
-
-
 export const HeaderBar = () => {
-  
   const { push } = useHistory()
-  const { location } = useLocation()
+  const location = useLocation()
   //Navbar Places
-  const [showPlacesbar, setShowPlacesbar] = useState(false)
+  const [showPlacesbar, setShowPlacesbar] = useState(null)
   //Dropdownbar
   const [isOpen, setIsOpen] = useState(false)
   const toggle = () => setIsOpen(!isOpen)
-  //Place in home page
-  const [showPlace, setShowPlace] = useState(false)
   //Button About Us in home page
   const [showBtnAbout, setShowBtnAbout] = useState(null)
 
   useEffect(() => {
-    console.log('locaton',location)
-    // localStorage.removeItem('setBtn')
-    console.log('setBtn',localStorage.getItem('setBtn'))
-    if (localStorage.getItem('setBtn') == null) {
+    checkPage()
+  }, [location.pathname])
+  const checkPage = () => {
+    if (location.pathname == '/') {
       setShowBtnAbout(true)
-    } else {
+      setShowPlacesbar(false)
+    } else if (location.pathname == '/AboutUs') {
       setShowBtnAbout(false)
-      localStorage.removeItem('setBtn')
+      setShowPlacesbar(false)
+    } else {
+      setShowBtnAbout(true)
+      setShowPlacesbar(true)
     }
-    // {showBtnAbout: true} : localStorage.getItem('setBtn') == 'false' ? {showBtnAbout: false} : {showBtnAbout: true}
-  }, [])
-
-  
+  }
 
   const AboutusClick = () => {
     push('./AboutUs')
     setShowBtnAbout(false)
-    localStorage.setItem('setBtn', false)
   }
 
   const Place1Click = () => {
     push('./Place1')
     setShowPlacesbar(true)
-    setShowPlace(false)
     setShowBtnAbout(true)
   }
 
   const Place2Click = () => {
-    push('./Place2')
-    setShowPlace(false)
+    push({ pathname: './Place2' })
     setShowBtnAbout(true)
     setShowPlacesbar(true)
   }
 
   return (
-    <div >
-
-      <Navbar color="dark" dark expand="md" >
-        <NavbarBrand color="dark" href="/">SmartParking</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        { showPlacesbar &&
-        <Collapse isOpen={isOpen} navbar>
-        <Nav navbar className="ml-auto"  >
-          <UncontrolledDropdown nav inNavbar >
-            <DropdownToggle nav caret >
-              Places
-            </DropdownToggle>
-            <DropdownMenu right>
-              <DropdownItem onClick={Place1Click}>Place 1</DropdownItem>
-              <DropdownItem divider />
-              <DropdownItem onClick={Place2Click}>Place 2</DropdownItem>
-            </DropdownMenu>
-          </UncontrolledDropdown>
-        </Nav>
-        </Collapse>
-        }
+    <div>
+      <Navbar color="dark" dark expand="md">
+        <NavbarBrand color="dark" href="/">
+          SmartParking
+        </NavbarBrand>
+        {showPlacesbar && <NavbarToggler onClick={toggle} />}
+        {showPlacesbar && (
+          <Collapse isOpen={isOpen} navbar>
+            <Nav navbar className="ml-auto">
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Places
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem onClick={Place1Click}>Place 1</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem onClick={Place2Click}>Place 2</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        )}
       </Navbar>
-      
-      {
-        showBtnAbout &&
-      
-      <div>
-        <Button className="btnAbout" onClick={AboutusClick}>
-          About Us
-        </Button>
-      </div>
-       }
+
+      {showBtnAbout && (
+        <div>
+          <Button className="btnAbout" onClick={AboutusClick}>
+            About Us
+          </Button>
+        </div>
+      )}
     </div>
   )
 }

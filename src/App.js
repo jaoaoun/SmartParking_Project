f00e944379c95810bea564aae2fe410a71c4bd3e
o.srч <sorayuth10@gmail.com>
2020-04-27 17:07:09 +0700

    component fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 import Home from './components/page/Home'
 import Place1 from './components/page/Place1'
 import Place2 from './components/page/Place2'
 import AboutUs from './components/page/AboutUs'
 import HeaderBar from './components/layout/HeaderBar'
-import HeaderplaceBar from './components/layout/HeaderplaceBar'
-import BtnAbout from './components/layout/BtnAbout'
 
 function App() {
-  /* Hello */
-
   return (
     <Router>
       <div>
         <Switch>
           <Route path="/Place2">
             <HeaderBar />
-            
             <Place2 />
           </Route>
           <Route path="/Place1">

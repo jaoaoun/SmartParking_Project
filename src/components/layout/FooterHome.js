5680136c15cf87ed7cbf5499c5d70f1229908741
jaoaoun <ntd_pst@hotmail.com>
2020-04-29 20:47:48 +0700

    Create FooterHome, revise Footer,un i semantic

--- src/components/layout/FooterHome.js
+++ src/components/layout/FooterHome.js
@@ -1,37 +1,70 @@
 import React from 'react'
-import '../../css/indexApp.css'
-import { Container, Row, Col, Card, CardTitle, CardText} from 'reactstrap'
+import '../../css/FooterHome.css'
+import { Container} from 'reactstrap'
+
 
 export const FooterHome = () => {
   return (
+    
+
+    // <footer className="footer fixed-bottom">
+    //   <div>
+    //     <p></p>
+    //     <Card.Group centered>
+    //       <Card fluid bg="dark" className="text-center p-3" md={6}>
+    //         <CardBody>
+    //           <CardTitle>Download App Free</CardTitle>
+    //           <CardContent>
+    //             <Row align="center">
+    //               <Col>
+    //                 <div>
+    //                   <Card>
+    //                     <CardTitle>IOS</CardTitle>
+    //                     <img src={require('../../img/Grey.png')} alt="logo" className="roecar" />
+    //                   </Card>
+    //                 </div>
+    //               </Col>
+    //               <Col>
+    //                 <div>
+    //                   <Card></Card>
+    //                 </div>
+    //               </Col>
+    //             </Row>
+    //           </CardContent>
+    //         </CardBody>
+    //       </Card>
+    //     </Card.Group>
+    //   </div>
+
+    //   <div align="center">
+    //     <Container>
+    //       <p>Smart Parking Project &copy; 2020 Nattida P. All right reserved.</p>
+    //     </Container>
+    //   </div>
+    // </footer>
     <div>
-      
-        <div className="Beforeft-home">
-          <Container >
-            <Row>
-              <Col sm="6" >
-                <Card body>
-                  <CardTitle>IOS</CardTitle>
-                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                </Card>
-              </Col>
-              <Col sm="6">
-                <Card body>
-                  <CardTitle>ANDRIOD</CardTitle>
-                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                </Card>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-        <footer className="footer">
-        <div >
-            <Container>
-                
-            </Container>
-        </div>
-      </footer>
-    </div>
+    <footer className="footer fixed-bottom">
+      <div className="div-footer">
+        <Container>
+          
+            <div align="center">
+              <span>
+                 <img src={require('../../img/kmi.jpg')} alt="logo" className="img-footerhome" />
+                <img src={require('../../img/ite-logo.png')} alt="logo" className="img-footerhome" />
+              </span>
+               
+            </div>
+        
+        </Container>
+      </div>
+      <hr className="hr-footer"></hr>
+      <div align="center">
+        <Container>
+          <p className="copyright">Smart Parking Project &copy; 2020 Nattida P. All right reserved.</p>
+        </Container>
+      </div>
+    </footer>
+  </div>
   )
 }
 export default FooterHome
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import * as firebase from 'firebase'
 import { firebaseConfig } from './config'
 firebase.initializeApp(firebaseConfig)
 
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')

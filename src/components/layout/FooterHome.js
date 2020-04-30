a714753cc973fabc5135fca07e5dba1453c13771
jaoaoun <ntd_pst@hotmail.com>
2020-04-30 21:37:37 +0700

    Add create card download app,format code

--- src/components/layout/FooterHome.js
+++ src/components/layout/FooterHome.js
@@ -1,70 +1,58 @@
 import React from 'react'
-import '../../css/FooterHome.css'
-import { Container} from 'reactstrap'
-
+import '../../css/HeaderFooter.css'
+import { Container, Card, CardBody, CardTitle, CardSubtitle, CardContent, Row, Col } from 'reactstrap'
 
 export const FooterHome = () => {
   return (
-    
-
-    // <footer className="footer fixed-bottom">
-    //   <div>
-    //     <p></p>
-    //     <Card.Group centered>
-    //       <Card fluid bg="dark" className="text-center p-3" md={6}>
-    //         <CardBody>
-    //           <CardTitle>Download App Free</CardTitle>
-    //           <CardContent>
-    //             <Row align="center">
-    //               <Col>
-    //                 <div>
-    //                   <Card>
-    //                     <CardTitle>IOS</CardTitle>
-    //                     <img src={require('../../img/Grey.png')} alt="logo" className="roecar" />
-    //                   </Card>
-    //                 </div>
-    //               </Col>
-    //               <Col>
-    //                 <div>
-    //                   <Card></Card>
-    //                 </div>
-    //               </Col>
-    //             </Row>
-    //           </CardContent>
-    //         </CardBody>
-    //       </Card>
-    //     </Card.Group>
-    //   </div>
-
-    //   <div align="center">
-    //     <Container>
-    //       <p>Smart Parking Project &copy; 2020 Nattida P. All right reserved.</p>
-    //     </Container>
-    //   </div>
-    // </footer>
     <div>
-    <footer className="footer fixed-bottom">
-      <div className="div-footer">
-        <Container>
-          
-            <div align="center">
-              <span>
-                 <img src={require('../../img/kmi.jpg')} alt="logo" className="img-footerhome" />
-                <img src={require('../../img/ite-logo.png')} alt="logo" className="img-footerhome" />
-              </span>
-               
-            </div>
-        
-        </Container>
+      <Container fluid>
+        <Card className="text-center bg-dark" md={6}>
+          <CardBody className="pb-4">
+            <CardTitle className="text-white">Do you want to book a park?</CardTitle>
+            <CardSubtitle className="text-white pb-3">Download Now! Smart Parking Mobile App.</CardSubtitle>
+            <Row align="center">
+              <Col>
+                <div>
+                  <Card className="contentApp">
+                    <CardTitle>IOS</CardTitle>
+                    <img src={require('../../img/Apple.png')} alt="logo" className="cardimg-download" />
+                  </Card>
+                </div>
+              </Col>
+              <Col>
+                <div>
+                  <Card className="contentApp">
+                    <CardTitle>Andriod</CardTitle>
+                    <img src={require('../../img/Playapp.png')} alt="logo" className="cardimg-download" />
+                  </Card>
+                </div>
+              </Col>
+            </Row>
+          </CardBody>
+        </Card>
+      </Container>
+
+      <div>
+        <footer className="footer">
+          <div className="div-footer">
+            <Container>
+              <div align="center">
+                <span>
+                  <img src={require('../../img/kmi.jpg')} alt="logo" className="img-footerhome" />
+                  <img src={require('../../img/ite-logo.png')} alt="logo" className="img-footerhome" />
+                </span>
+              </div>
+            </Container>
+          </div>
+          <hr className="hr-footer"></hr>
+          <div align="center">
+            <Container>
+              <p className="copyright">Smart Parking Project &copy; 2020 Nattida P. => SRY. All right reserved.</p>
+            </Container>
+          </div>
+        </footer>
       </div>
-      <hr className="hr-footer"></hr>
-      <div align="center">
-        <Container>
-          <p className="copyright">Smart Parking Project &copy; 2020 Nattida P. All right reserved.</p>
-        </Container>
-      </div>
-    </footer>
-  </div>
+    </div>
   )
 }
 export default FooterHome

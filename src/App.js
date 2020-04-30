eb3125c1eb5c2ecc43611a08001c7331f613962d
jaoaoun <ntd_pst@hotmail.com>
2020-04-30 19:22:02 +0700

    import Footer and remove key

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Home from './components/page/Home'
 import Place1 from './components/page/Place1'
@@ -7,28 +7,34 @@ import Place2 from './components/page/Place2'
 import AboutUs from './components/page/AboutUs'
 import HeaderBar from './components/layout/HeaderBar'
 import FooterHome from './components/layout/FooterHome'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router>
       <div>
         <Switch>
+
+          
           <Route path="/Place2">
             <HeaderBar />
             <Place2 />
+            <Footer />
           </Route>
           <Route path="/Place1">
             <HeaderBar />
             <Place1 />
+            <Footer />
           </Route>
           <Route path="/AboutUs">
             <HeaderBar />
             <AboutUs />
+            <Footer />
           </Route>
           <Route path="/">
-            <HeaderBar key="1" />
-            <Home key="3" />
-            <FooterHome key="2"/>
+            <HeaderBar />
+            <Home />
+            <FooterHome />
           </Route>
         </Switch>
       </div>

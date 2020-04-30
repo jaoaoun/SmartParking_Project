08bcb4df5aa7fca9d400a7362ed7ba5e2253b00f
jaoaoun <ntd_pst@hotmail.com>
2020-04-30 16:46:36 +0700

    Revise Place2 same as Place1

--- src/components/page/Place2.js
+++ src/components/page/Place2.js
@@ -23,30 +23,26 @@ export const Place2 = () => {
         <Row>
           <h1>Place2</h1>
         </Row>
-        <div className="row no-gutter">
-        <Row >
-          <Col sm={2} align="center">
+        <div className="row no-gutter justify-content-center">
+        <Row  >
+          <Col  sm={2} align="center">
             <p className="No-car">001</p>
             <img src={require('../../img/Grey.png')} alt="logo" />
           </Col>
-          <Col sm={2} align="center">
+          <Col  sm={2} align="center">
             <p className="No-car">002</p>
             <img src={require('../../img/Grey.png')} alt="logo" />
           </Col>
-          <Col sm={2} align="center">
+          <Col  sm={2} align="center">
             <p className="No-car">003</p>
             <img src={require('../../img/Grey.png')} alt="logo" />
           </Col>
-          <Col sm={2} align="center">
+          <Col  sm={2} align="center">
             <p className="No-car">004</p>
             <img src={require('../../img/Grey.png')} alt="logo" />
           </Col>
-          <Col sm={2} align="center">
+          <Col  sm={2} align="center">
             <p className="No-car">005</p>
-            <img src={require('../../img/Grey.png')} alt="logo" />
-          </Col>
-          <Col sm={2} align="center">
-            <p className="No-car">006</p>
             <img src={require('../../img/Grey.png')} alt="logo" />
           </Col>
         </Row>
@@ -55,31 +51,27 @@ export const Place2 = () => {
         <Row className="gap">
         </Row>
         </div>
-        <div className="row no-gutter">
+        <div className="row no-gutter justify-content-center">
         <Row>
-          <Col sm={2} align="center">
+          <Col  sm={2} align="center">
+            <img src={require('../../img/Grey.png')} alt="logo" className="roecar" />
+            <p className="No-car">006</p>
+          </Col>
+          <Col  sm={2} align="center">
             <img src={require('../../img/Grey.png')} alt="logo" className="roecar" />
             <p className="No-car">007</p>
           </Col>
-          <Col sm={2} align="center">
+          <Col  sm={2} align="center">
             <img src={require('../../img/Grey.png')} alt="logo" className="roecar" />
             <p className="No-car">008</p>
           </Col>
-          <Col sm={2} align="center">
+          <Col  sm={2} align="center">
             <img src={require('../../img/Grey.png')} alt="logo" className="roecar" />
             <p className="No-car">009</p>
           </Col>
-          <Col sm={2} align="center">
+          <Col  sm={2} align="center">
             <img src={require('../../img/Grey.png')} alt="logo" className="roecar" />
             <p className="No-car">010</p>
-          </Col>
-          <Col sm={2} align="center">
-            <img src={require('../../img/Grey.png')} alt="logo" className="roecar" />
-            <p className="No-car">011</p>
-          </Col>
-          <Col sm={2} align="center">
-            <img src={require('../../img/Grey.png')} alt="logo" className="roecar" />
-            <p className="No-car">012</p>
           </Col>
         </Row>
         </div>

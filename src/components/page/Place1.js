d58ee716ea164370e5b941840e5f2178953f334b
o.srч <sorayuth10@gmail.com>
2020-05-02 01:00:53 +0700

    add PlaceInfo [map]

--- src/components/page/Place1.js
+++ src/components/page/Place1.js
@@ -1,9 +1,13 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../../css/indexApp.css'
-import { Container, Row, Col } from 'reactstrap'
+import { Container, Row, Col, Modal, ModalHeader, ModalBody, ModalFooter, Button} from 'reactstrap'
 import * as firebase from 'firebase'
+import Iframe from 'react-iframe'
 
 export const Place1 = () => {
+  const [modal, setModal] = useState(false)
+  const toggle = () => setModal(!modal)
+
   const fetchingSensor = () => {
     firebase
       .database()
@@ -22,7 +26,30 @@ export const Place1 = () => {
       <Container>
         <Row>
           <h1>Place1</h1>
+          <button type="button" class="btn btn-outline-info ml-5" onClick={toggle}>Info</button>
         </Row>
+        <Modal isOpen={modal} toggle={toggle} size="lg">
+          <ModalHeader toggle={toggle}>Place Info</ModalHeader>
+          <ModalBody>
+            <div class="d-flex justify-content-center">
+              <Iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15503.40165234832!2d100.7723961!3d13.7275048!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd2e8705e1eebfc3e!2z4Lit4Liy4LiE4Liy4LijIDEyIOC4iuC4seC5ieC4mSDguKrguJbguLLguJrguLHguJnguYDguJfguITguYLguJnguYLguKXguKLguLXguJ7guKPguLDguIjguK3guKHguYDguIHguKXguYnguLLguYDguIjguYnguLLguITguLjguJPguJfguKvguLLguKPguKXguLLguJTguIHguKPguLDguJrguLHguIc!5e0!3m2!1sth!2sth!4v1588334447113!5m2!1sth!2sth"
+                width="600"
+                height="450"
+                frameborder="0"
+                style="border:0;"
+                allowfullscreen=""
+                aria-hidden="false"
+                tabindex="0"
+              />
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={toggle}>
+              Close
+            </Button>
+          </ModalFooter>
+        </Modal>
         <div className="row no-gutter justify-content-center">
           <Row>
             <Col sm={2} align="center">

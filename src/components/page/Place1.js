713006690fa3f13ef284d76800af987f403ab910
o.srч <sorayuth10@gmail.com>
2020-05-05 14:04:30 +0700

    axios fetching data and car parking report

--- src/components/page/Place1.js
+++ src/components/page/Place1.js
@@ -1,43 +1,52 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useCallback, useState } from 'react'
 import '../../css/indexApp.css'
 import { Container, Row, Col, Card, CardBody, CardHeader } from 'reactstrap'
-import * as firebase from 'firebase'
+import axios from 'axios'
 import Iframe from 'react-iframe'
 
 export const Place1 = () => {
-
-  const fetchingSensor = () => {
-    firebase
-      .database()
-      .ref('Devices/Node-001')
-      .once('value', (data) => {
-        console.log(data.toJSON().Sensor)
-      })
+  const [carPark, setCarPark] = useState([])
+  const parkReport = (num) => {
+    if (carPark[num] === 0) {
+      return 'Red'
+    } else if (carPark[num] === 1) {
+      return 'Green'
+    } else {
+      return 'Grey'
+    }
   }
 
+  const fetchingSensor = useCallback(async () => {
+    let sensor = await axios.get(`http://127.0.0.1:5000/sensor/`) //http://34.87.153.90:5000/sensor
+    setCarPark(sensor.data)
+  }, [])
+
   useEffect(() => {
-    fetchingSensor()
-  })
+    setTimeout(() => {
+      fetchingSensor()
+    }, 4000)
+    parkReport()
+  }, [fetchingSensor, parkReport])
 
   return (
     <div>
       <Container>
         <Row>
-          <h1>Place1</h1>
+          <h1>Convention Hall</h1>
         </Row>
         <div className="row no-gutter justify-content-center">
           <Row>
             <Col sm={2.4} align="center">
               <p className="No-car">001</p>
-              <img src={require('../../img/Grey.png')} alt="logo" />
+              <img src={require(`../../img/${parkReport(0)}.png`)} alt="logo" />
             </Col>
             <Col sm={2.4} align="center">
               <p className="No-car">002</p>
-              <img src={require('../../img/Grey.png')} alt="logo" />
+              <img src={require(`../../img/${parkReport(1)}.png`)} alt="logo" />
             </Col>
             <Col sm={2.4} align="center">
               <p className="No-car">003</p>
-              <img src={require('../../img/Grey.png')} alt="logo" />
+              <img src={require(`../../img/${parkReport(2)}.png`)} alt="logo" />
             </Col>
             <Col sm={2.4} align="center">
               <p className="No-car">004</p>
@@ -77,22 +86,22 @@ export const Place1 = () => {
           </Row>
         </div>
         <div align="center" className="mt-5">
-            <Col sm={6}>
-              <Card >
-                <CardHeader>Location</CardHeader>
-                <CardBody>
+          <Col sm={6}>
+            <Card>
+              <CardHeader>Location</CardHeader>
+              <CardBody>
                 <Iframe
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15503.40165234832!2d100.7723961!3d13.7275048!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd2e8705e1eebfc3e!2z4Lit4Liy4LiE4Liy4LijIDEyIOC4iuC4seC5ieC4mSDguKrguJbguLLguJrguLHguJnguYDguJfguITguYLguJnguYLguKXguKLguLXguJ7guKPguLDguIjguK3guKHguYDguIHguKXguYnguLLguYDguIjguYnguLLguITguLjguJPguJfguKvguLLguKPguKXguLLguJTguIHguKPguLDguJrguLHguIc!5e0!3m2!1sth!2sth!4v1588334447113!5m2!1sth!2sth"
-                width="100%"
-                height="300"
-                frameborder="0"
-                allowfullscreen=""
-                aria-hidden="false"
-                tabindex="0"
-              />
-                </CardBody>
-              </Card>
-              </Col>
+                  src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15503.40165234832!2d100.7723961!3d13.7275048!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd2e8705e1eebfc3e!2z4Lit4Liy4LiE4Liy4LijIDEyIOC4iuC4seC5ieC4mSDguKrguJbguLLguJrguLHguJnguYDguJfguITguYLguJnguYLguKXguKLguLXguJ7guKPguLDguIjguK3guKHguYDguIHguKXguYnguLLguYDguIjguYnguLLguITguLjguJPguJfguKvguLLguKPguKXguLLguJTguIHguKPguLDguJrguLHguIc!5e0!3m2!1sth!2sth!4v1588334447113!5m2!1sth!2sth"
+                  width="100%"
+                  height="300"
+                  frameborder="0"
+                  allowfullscreen=""
+                  aria-hidden="false"
+                  tabindex="0"
+                />
+              </CardBody>
+            </Card>
+          </Col>
         </div>
       </Container>
     </div>
--- src/components/page/Place2.js
+++ src/components/page/Place2.js
@@ -1,34 +1,44 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useCallback, useState } from 'react'
 import '../../css/indexApp.css'
 import { Container, Row, Col, Card, CardHeader, CardBody } from 'reactstrap'
-import * as firebase from 'firebase'
+import axios from 'axios'
 import Iframe from 'react-iframe'
 
 export const Place2 = () => {
-  const fetchingSensor = () => {
-    firebase
-      .database()
-      .ref('Devices/Node-001')
-      .once('value', (data) => {
-        console.log(data.toJSON().Sensor)
-      })
+  const [carPark, setCarPark] = useState([])
+  const parkReport = (num) => {
+    if (carPark[num] === 0) {
+      return 'Red'
+    } else if (carPark[num] === 1) {
+      return 'Green'
+    } else {
+      return 'Grey'
+    }
   }
 
+  const fetchingSensor = useCallback(async () => {
+    const sensor = await axios.get(`http://127.0.0.1:5000/sensor/`) //http://34.87.153.90:5000/sensor
+    setCarPark(sensor.data)
+  }, [])
+
   useEffect(() => {
-    fetchingSensor()
-  })
+    setTimeout(() => {
+      fetchingSensor()
+    }, 5000)
+    parkReport()
+  }, [fetchingSensor, parkReport])
 
   return (
     <div>
       <Container>
         <Row>
-          <h1>Place2</h1>
+          <h1>12 Building Floor</h1>
         </Row>
         <div className="row no-gutter justify-content-center">
           <Row>
             <Col sm={2.4} align="center">
               <p className="No-car">001</p>
-              <img src={require('../../img/Grey.png')} alt="logo" />
+              <img src={require(`../../img/${parkReport(3)}.png`)} alt="logo" />
             </Col>
             <Col sm={2.4} align="center">
               <p className="No-car">002</p>
@@ -76,22 +86,23 @@ export const Place2 = () => {
           </Row>
         </div>
         <div align="center" className="mt-5">
-            <Col sm={6}>
-              <Card >
-                <CardHeader>Location</CardHeader>
-                <CardBody>
+          <Col sm={6}>
+            <Card>
+              <CardHeader>Location</CardHeader>
+              <CardBody>
                 <Iframe
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15503.40165234832!2d100.7723961!3d13.7275048!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd2e8705e1eebfc3e!2z4Lit4Liy4LiE4Liy4LijIDEyIOC4iuC4seC5ieC4mSDguKrguJbguLLguJrguLHguJnguYDguJfguITguYLguJnguYLguKXguKLguLXguJ7guKPguLDguIjguK3guKHguYDguIHguKXguYnguLLguYDguIjguYnguLLguITguLjguJPguJfguKvguLLguKPguKXguLLguJTguIHguKPguLDguJrguLHguIc!5e0!3m2!1sth!2sth!4v1588334447113!5m2!1sth!2sth"
-                width="100%"
-                height="300"
-                frameborder="0"
-                allowfullscreen=""
-                aria-hidden="false"
-                tabindex="0"
-              />
-                </CardBody>
-              </Card>
-              </Col>
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.8619671050146!2d100.77695021483021!3d13.726805590363574!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTPCsDQzJzM2LjUiTiAxMDDCsDQ2JzQ0LjkiRQ!5e0!3m2!1sth!2sth!4v1588661034886!5m2!1sth!2sth"
+                  width="100%"
+                  height="300"
+                  frameborder="0"
+                  allowfullscreen=""
+                  aria-hidden="false"
+                  tabindex="0"
+                ></Iframe>
+                />
+              </CardBody>
+            </Card>
+          </Col>
         </div>
       </Container>
     </div>

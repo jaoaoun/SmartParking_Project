491093bae9bda60b49dfba58e4efa9cad1df4b12
o.srч <sorayuth10@gmail.com>
2020-05-05 14:03:11 +0700

    del Link

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import '../../css/Footer.css'
 import { Container, Row, Col } from 'reactstrap'
-import { Link } from 'react-router-dom'
 
 export const Footer = () => {
   return (

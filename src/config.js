d6bd1256bfcf49a1b6487f0c3daaa02d1400a121
o.srч <sorayuth10@gmail.com>
2020-06-02 22:56:43 +0700

    add config-env

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: REACT_APP_AUTHDOMAIN,
+  databaseURL: REACT_APP_DATABASE_URL,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGEBUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APPID,
+  measurementId: REACT_APP_MEASUREMENTID
+}

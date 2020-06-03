48c22b64c9d47dedbe4f70627a3cecd5900e20d5
o.srч <sorayuth10@gmail.com>
2020-06-03 22:43:48 +0700

    config env edited

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
   apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: REACT_APP_AUTHDOMAIN,
-  databaseURL: REACT_APP_DATABASE_URL,
-  projectId: REACT_APP_PROJECT_ID,
-  storageBucket: REACT_APP_STORAGEBUCKET,
-  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
-  appId: REACT_APP_APPID,
-  measurementId: REACT_APP_MEASUREMENTID
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID
 }
